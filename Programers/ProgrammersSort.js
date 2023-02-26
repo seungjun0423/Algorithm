1dacc83e6ce195985dbbbff08126b9ba29a64ba3
seungjun0423 <sibonum@gmail.com>
2023-02-27 08:29:27 +0900

    sorting solved

--- Programers/ProgrammersSort.js
+++ Programers/ProgrammersSort.js
@@ -9,3 +9,11 @@ numbers의 길이는 1 이상 100,000 이하입니다.
 numbers의 원소는 0 이상 1,000 이하입니다.
 정답이 너무 클 수 있으니 문자열로 바꾸어 return 합니다.
 */
+
+function solution(numbers) {
+    var answer = numbers.map(v=>v+'')
+                        .sort((a,b) => (b+a)*1 - (a+b)*1)
+                        .join('');
+
+    return answer[0]==='0'?'0':answer;
+}
