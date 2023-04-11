4f65df7a587323568f974c3cf0820642ba769388
seungjun0423 <sibonum@gmail.com>
2023-04-12 00:51:43 +0900

    level2 solved

--- Programers/Programmers_level2.js
+++ Programers/Programmers_level2.js
@@ -404,8 +404,46 @@ groups.sort((a, b) => b - a);
 let maxScore = 0;
 	for (let i = 0; i < groups.length - 1; i++) {
  		const score = groups[i] * groups[i + 1];
-  	if (score > maxScore) maxScore = score;
-	}
+		if (score > maxScore) maxScore = score;
+	}''
 
 return maxScore;
+}
+
+
+/*
+다음 큰 숫자
+
+문제 설명
+자연수 n이 주어졌을 때, n의 다음 큰 숫자는 다음과 같이 정의 합니다.
+조건 1. n의 다음 큰 숫자는 n보다 큰 자연수 입니다.
+조건 2. n의 다음 큰 숫자와 n은 2진수로 변환했을 때 1의 갯수가 같습니다.
+조건 3. n의 다음 큰 숫자는 조건 1, 2를 만족하는 수 중 가장 작은 수 입니다.
+예를 들어서 78(1001110)의 다음 큰 숫자는 83(1010011)입니다.
+자연수 n이 매개변수로 주어질 때, n의 다음 큰 숫자를 return 하는 solution 함수를 완성해주세요.
+
+제한 사항
+n은 1,000,000 이하의 자연수 입니다.
+*/
+
+function solution(n) {
+	const countOnes = (binStr) => {
+			return binStr.split('').reduce((acc, curr) => acc + (curr === '1' ? 1 : 0), 0);
+	};
+
+	const nBinary = n.toString(2);
+	const nOnesCount = countOnes(nBinary);
+
+	let nextNum = n + 1;
+
+	while (true) {
+			const nextNumBinary = nextNum.toString(2);
+			const nextNumOnesCount = countOnes(nextNumBinary);
+
+			if (nOnesCount === nextNumOnesCount) {
+					return nextNum;
+			}
+
+			nextNum += 1;
+	}
 }
