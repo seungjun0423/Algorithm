d460620fc907de5b890dd643ebc33efa8f441fcb
seungjun0423 <sibonum@gmail.com>
2023-04-14 08:55:00 +0900

    스택을이용

--- Programers/Programmers_level2.js
+++ Programers/Programmers_level2.js
@@ -452,7 +452,7 @@ function solution(n) {
 피보 나치 함수를 작성하는 다양한 방법 (재귀방식은 시간 복잡도도 크고 간단하기 때문에 생략한다)
 */
 
-// 메모이제이션을 이용해 구하는 방법 (시간 복잡도 O(n))
+// 재귀를 이용하지만 메모이제이션을 이용해 시간복잡도를 감소시킨 방법 (시간 복잡도 O(n), 일반 재귀는 시간복잡도 O(n**2))
 function solution(n) {
 	const memo = {};
 
@@ -470,26 +470,8 @@ return fib(n)%1234567;
 	
 } 
 
-
+// 최종 통과 알고리즘 
 // 반복문을 이용해 구하는 방법 (시간 복잡도는 O(n))
-function fibonacci(n) {
-  if (n <= 1) return n;
-
-  let prevPrev = 0;
-  let prev = 1;
-  let current;
-
-  for (let i = 2; i <= n; i++) {
-    current = prevPrev + prev;
-    prevPrev = prev;
-    prev = current;
-  }
-
-  return current;
-}
-
-
-// 최종 통과 알고리즘
 function solution(n) {
   // n 번째 피보나치 수를 1234567로 나눈 나머지를 리턴하는 함수를 구하라
   // 1. n번째 피보나치 수를 구하는 함수 : 재귀함수는 안된다.
@@ -506,4 +488,29 @@ function solution(n) {
   }
 
   return acc % 1234567;
+}
+
+/*
+짝지어 제거하기
+
+문제 설명
+짝지어 제거하기는, 알파벳 소문자로 이루어진 문자열을 가지고 시작합니다. 먼저 문자열에서 같은 알파벳이 2개 붙어 있는 짝을 찾습니다. 그다음, 그 둘을 제거한 뒤, 앞뒤로 문자열을 이어 붙입니다. 이 과정을 반복해서 문자열을 모두 제거한다면 짝지어 제거하기가 종료됩니다. 문자열 S가 주어졌을 때, 짝지어 제거하기를 성공적으로 수행할 수 있는지 반환하는 함수를 완성해 주세요. 성공적으로 수행할 수 있으면 1을, 아닐 경우 0을 리턴해주면 됩니다.
+예를 들어, 문자열 S = baabaa 라면
+b aa baa → bb aa → aa →
+의 순서로 문자열을 모두 제거할 수 있으므로 1을 반환합니다.
+
+제한사항
+문자열의 길이 : 1,000,000이하의 자연수
+문자열은 모두 소문자로 이루어져 있습니다.
+*/
+
+function solution(s) {
+	const stack = [];
+
+		for(let i = 0 ; i < s.length ; i++ ){
+			if( !stack.length || stack[stack.length-1] !== s[i] ) stack.push(s[i]);
+				else stack.pop();
+	}
+
+return stack.length ? 0 : 1;
 }
