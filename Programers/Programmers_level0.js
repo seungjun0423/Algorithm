886184a3fdcc38e8aa079f696ee3dcb7984f903d
seungjun0423 <sibonum@gmail.com>
2023-05-15 12:00:49 +0900

    split()을 이용한 여집합 문제 풀이

--- Programers/Programmers_level0.js
+++ Programers/Programmers_level0.js
@@ -117,3 +117,35 @@ function solution(n) {
 	while (f*i < n) f*=++i;
 	return i;
 }
+
+/**
+k의 개수
+문제 설명
+1부터 13까지의 수에서, 1은 1, 10, 11, 12, 13 이렇게 총 6번 등장합니다. 
+정수 i, j, k가 매개변수로 주어질 때, i부터 j까지 k가 몇 번 등장하는지 return 하도록 solution 함수를 완성해주세요.
+ */
+
+// 나의 풀이 
+function solution(i, j, k) {
+	let arr = [];
+	
+	for(let t=i;t<=j;t++){
+			if(String(t).includes(String(k))){
+				arr.push(String(t));   
+			}
+	}
+	arr = arr.map(el=>{let list = el.split('');return list.filter(value => value === String(k)).length;})
+	return arr.reduce((pre,cur)=>pre+cur,0);
+}
+
+
+
+// 참고할만한 다른 사람의 풀이 => split을 이용한 여집합 : split의 인자 k를 기준으로 나누어 지며 이 경우 k의 수 보다 항상 1이 많기 때문에 -1을 한다. 
+function solution(i, j, k) {
+	let a ='';
+	for(i;i<=j;i++){
+			a += i;
+	}
+
+	return a.split(k).length-1;
+}
