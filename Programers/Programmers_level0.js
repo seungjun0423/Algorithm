5c2e11802ef1e74a6d34fe2cf070ae3e7276d110
seungjun0423 <sibonum@gmail.com>
2023-05-24 17:58:20 +0900

    삼각형의 완성조건

--- Programers/Programmers_level0.js
+++ Programers/Programmers_level0.js
@@ -149,3 +149,44 @@ function solution(i, j, k) {
 
 	return a.split(k).length-1;
 }
+
+
+/**
+숨어있는 숫자의 덧셈 (2)
+문제 설명
+문자열 my_string이 매개변수로 주어집니다. my_string은 소문자, 대문자, 자연수로만 구성되어있습니다. my_string안의 자연수들의 합을 return하도록 solution 함수를 완성해주세요.
+ */
+
+// 정규 표현식을 이용해 문제를 풀었다.
+
+function solution(my_string) {
+	let regEng = /^[a-zA-Z]*$/;
+	let sum = '';
+	for(let i=0;i<my_string.length;i++){
+			let el = my_string[i];
+			if(regEng.test(el)){
+					sum += ' ';
+			} else {
+					sum += el;
+			}
+	}
+	return sum.split(' ').reduce((pre,cur)=>Number(pre)+Number(cur),0)
+}
+
+/**
+삼각형의 완성조건
+
+문제 설명
+선분 세 개로 삼각형을 만들기 위해서는 다음과 같은 조건을 만족해야 합니다.
+가장 긴 변의 길이는 다른 두 변의 길이의 합보다 작아야 합니다.
+삼각형의 두 변의 길이가 담긴 배열 sides이 매개변수로 주어집니다. 나머지 한 변이 될 수 있는 정수의 개수를 return하도록 solution 함수를 완성해주세요.
+
+제한사항
+sides의 원소는 자연수입니다.
+sides의 길이는 2입니다.
+1 ≤ sides의 원소 ≤ 1,000
+ */
+
+function solution(sides) {
+	return Math.min(...sides)*2-1
+}
