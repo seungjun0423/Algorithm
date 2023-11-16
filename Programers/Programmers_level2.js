0c3fe20778933ddad51ba3186d91941276f9e617
이승준 <87581422+seungjun0423@users.noreply.github.com>
2023-11-16 21:55:21 +0900

    시간 복잡도 개선

--- Programers/Programmers_level2.js
+++ Programers/Programmers_level2.js
@@ -545,3 +545,32 @@ function solution(k, tangerine) {
 	}
 	return result;
 }
+
+// 시간 초과 개선
+function solution(k, tangerine) {
+	var answer = 0;
+	const countTangerine = {}
+	tangerine.forEach(tan=>{
+			if(countTangerine[tan] === undefined){
+					countTangerine[tan] = 1
+			}
+			else{
+					countTangerine[tan]++
+			}
+	})
+
+	const tangerineGroup = Object.entries(countTangerine).map(([key,value])=>({key,value}))
+
+	tangerineGroup.sort((a,b)=>b.value - a.value)
+	for(let i=0;i<tangerineGroup.length;i++){
+			if(k - tangerineGroup[i].value <= 0){
+				 answer++
+					break;
+			}
+			else{
+					k -= tangerineGroup[i].value
+					answer++
+			}
+	}
+	return answer;
+}
